5c2ce45bd7246b59fab0d62d5ce6cb5944cfa569
Viktor Penelski <viktor.penelski@hyperscience.com>
2021-12-02 15:35:03 +0200

    single line lambda for boyan

--- day2/solution.py
+++ day2/solution.py
@@ -7,8 +7,7 @@ from day2.factories import DirectionsFactory, DirectionsFactoryAim, Factory
 
 def calculate(factory: Factory, lines: Sequence[str]) -> int:
     directions = [factory.parse_line(line) for line in lines]
-    last_reading = reduce(lambda latest_reading, direction:
-                          direction.move(latest_reading),
+    last_reading = reduce(lambda latest_reading, direction: direction.move(latest_reading),
                           directions,
                           Reading())
     return last_reading.multiplied()
