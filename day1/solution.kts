7e9fd213d69db1dd962d0899689b87f18a6863e9
Victor Penelski <victor.penelski@gmail.com>
2021-12-07 02:30:50 +0200

    simplify day1 in kotlin

--- day1/solution.kts
+++ day1/solution.kts
@@ -1,23 +1,7 @@
 import java.io.File
 
-fun solve(window: Int): Int {
-    var lastValues = ArrayDeque<Int>()
+fun inputAsInts() = File("in.txt").readLines().map { it.trim().toInt() }
+fun List<Int>.solve(window: Int) = this.windowed(window).count { it[window-1] > it[0] }
 
-    return File("in.txt").readLines()
-        .map<String, Int> { it.trim().toInt() }
-        .map<Int, Int> { currentValue ->
-            if (lastValues.size < window) {
-                lastValues.addLast(currentValue)
-                0
-            } else {
-                val prevWindow = lastValues.sum()
-                lastValues.removeFirst()
-                lastValues.addLast(currentValue)
-                val currWindow = lastValues.sum()
-                if (currWindow > prevWindow) 1 else 0
-            }
-        }.sum()
-}
-
-println(solve(window = 1))
-println(solve(window = 3))
+println(inputAsInts().solve(2))
+println(inputAsInts().solve(4))
