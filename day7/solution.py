c0d2825421df76fbe47b2d36bec4013e23c04172
Viktor Penelski <viktor.penelski@hyperscience.com>
2021-12-08 11:04:35 +0200

    fix day 7/8 mashup

--- day7/solution.py
+++ day7/solution.py
@@ -1,45 +1,15 @@
-from dataclasses import dataclass
 from functools import reduce
-from typing import List
+from typing import List, Callable
 
 
-@dataclass
-class RawDigit:
-    values: str
-    _known_lengths = {
-        1: 2,
-        4: 4,
-        7: 3,
-        8: 7
-    }
+def min_fuel(reduce_fun: Callable, positions: List[int]) -> int:
+    return min([reduce(reduce_fun(i), positions, 0) for i in range(max(positions))])
 
-    def check_for(self, digit: int):
-        return self._known_lengths.get(digit) == len(self.values)
-
-
-@dataclass
-class Reading:
-    inputs: List[RawDigit]
-    outputs: List[RawDigit]
 
 with open('in.txt') as f:
-    values = (line.strip() for line in f.readlines())
-    splits = (line.split(' ') for line in values)
-
-    readings: List[Reading] = []
-    for line in splits:
-        row_inputs = []
-        row_outputs = []
-        for i in range(10):
-            row_inputs.append(RawDigit(line[i]))
-        for i in range(11, 15):
-            row_outputs.append(RawDigit(line[i]))
-        readings.append(Reading(row_inputs, row_outputs))
-
-count_first = 0
-for r in readings:
-    for o in r.outputs:
-        if o.check_for(1) or o.check_for(4) or o.check_for(7) or o.check_for(8):
-            count_first+=1
-
-print(count_first)
+    _line = f.readline()
+    positions = list(map(int, _line.split(',')))
+cost_first = lambda idx: lambda acc, pos: acc + abs(pos - idx)
+cost_second = lambda idx: lambda acc, pos: acc + (abs(pos - idx) * (1 + abs(pos - idx)) / 2)
+print(min_fuel(cost_first, positions))
+print(min_fuel(cost_second, positions))
