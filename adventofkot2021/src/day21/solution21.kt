cf263b4194d1c118ef09017b118fb7afc8cde37a
Victor Penelski <victor.penelski@gmail.com>
2021-12-24 23:31:08 +0200

    minor refactor to d21

--- adventofkot2021/src/day21/solution21.kt
+++ adventofkot2021/src/day21/solution21.kt
@@ -1,10 +1,10 @@
 package day21
 
-data class Player(val position: Long, val pointsToWin: Long)
+data class Player(val position: Long, val points: Long = 0)
 
 val cache = mutableMapOf<Pair<Player, Player>, Pair<Long, Long>>()
 
-val rollFrequency = listOf(
+val rollFrequencies = mapOf(
     3 to 1,
     4 to 3,
     5 to 6,
@@ -15,27 +15,27 @@ val rollFrequency = listOf(
 )
 
 fun wins(p1: Player, p2: Player): Pair<Long, Long> {
-    if (p2.pointsToWin <= 0) return Pair(0, 1)
+    if (p2.points >= 21) return Pair(0, 1)
 
     val cached = cache[p1 to p2]
     if (cached != null) return cached
 
-    var wins1: Long = 0
-    var wins2: Long = 0
-    for ((roll, freq) in rollFrequency) {
+    val (wins1, wins2) = rollFrequencies.map { (roll, freq) ->
         val p1newPos = (p1.position + roll) % 10
         val (nextWins2, nextWins1) = wins(
             p2,
-            Player(p1newPos, p1.pointsToWin - p1newPos - 1)
+            Player(p1newPos, p1.points + p1newPos + 1)
         )
-        wins1 += freq * nextWins1
-        wins2 += freq * nextWins2
+        Pair(freq * nextWins1, freq * nextWins2)
+    }.reduce { acc, curWins ->
+        Pair(acc.first + curWins.first, acc.second + curWins.second)
     }
+
     cache[p1 to p2] = wins1 to wins2
     return wins1 to wins2
 }
 
 fun main() {
-    println(wins(Player(3, 21), Player(7, 21)))
-    println(wins(Player(7, 21), Player(9, 21)))
+    println(wins(Player(3), Player(7)))
+    println(wins(Player(7), Player(9)))
 }
